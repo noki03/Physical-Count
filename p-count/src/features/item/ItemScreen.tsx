ca96df1944fe7eac36f83007004d0a2296fd2599
rhys.salvador <rhys.salvador@noemail.cc>
2025-11-06 11:08:14 +0800

    chore: initial design for itemscreen

---
+++ p-count/src/features/item/ItemScreen.tsx
@@ -0,0 +1,12 @@
+import { Card } from "@/components/ui/card";
+import React from "react";
+
+const ItemScreen: React.FC = () => {
+  return (
+    <div className="w-full max-w-md mt-6">
+      <Card></Card>
+    </div>
+  );
+};
+
+export default ItemScreen;
